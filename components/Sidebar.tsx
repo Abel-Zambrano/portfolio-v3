9200047b830ad1f648fb7f090f7bfcc1da985746
Abel Zambrano <azambrano1417@gmail.com>
2022-02-01 21:15:01 -0800

    add image avatar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,21 +1,64 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 const MySidebar = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   width: 350px;
   height: 100%;
   background-color: var(--sidebar-bg);
+`;
+
+const AboutSection = styled.div`
+  background-color: gray; // todo: remove
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 400px;
 `;
 
 const Name = styled.h1`
   color: var(--heading);
 `;
 
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+
+  .image {
+    border-radius: 50%;
+  }
+`;
+
+const LinksSection = styled.div`
+  background-color: gray; // todo: remove
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 300px;
+`;
+
 export default function Sidebar() {
   return (
     <MySidebar>
-      <Name>Abel Zambrano</Name>
+      <AboutSection>
+        <Name>Abel Zambrano</Name>
+        <ImageWrapper>
+          <Image
+            className="image"
+            src="/az.png"
+            alt="portflio picture"
+            width={200}
+            height={200}
+            objectFit="cover"
+            priority
+          />
+        </ImageWrapper>
+      </AboutSection>
+      <LinksSection></LinksSection>
     </MySidebar>
   );
 }

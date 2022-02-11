4c632ee08a386a6b68ebb4f8dfb8fb89434d7090
Abel Zambrano <azambrano1417@gmail.com>
2022-02-11 10:59:01 -0800

    change logo to icons

--- JS/skills.js
+++ JS/skills.js
@@ -1,73 +1,89 @@
+import {
+  SiJavascript,
+  SiTypescript,
+  SiHtml5,
+  SiCss3,
+  SiReact,
+  SiRedux,
+  SiNextdotjs,
+  SiStyledcomponents,
+  SiSass,
+  SiGit,
+  SiMongodb,
+  SiMysql,
+  SiNodedotjs,
+  SiPython,
+} from "react-icons/si";
 const skills = [
   {
     id: 1,
     name: "javascript",
-    url: "https://live.staticflickr.com/65535/51871383674_a5c9a49a24_b.jpg",
+    icon: <SiJavascript />,
   },
   {
     id: 2,
     name: "typescript",
-    url: "https://live.staticflickr.com/65535/51870105752_dff6a15a8f_o.png",
+    icon: <SiTypescript />,
   },
   {
     id: 3,
     name: "html",
-    url: "https://live.staticflickr.com/65535/51871714935_28ccd9605d_o.png",
+    icon: <SiHtml5 />,
   },
   {
     id: 4,
     name: "css",
-    url: "https://live.staticflickr.com/65535/51871714940_84725e0c0f_b.jpg",
+    icon: <SiCss3 />,
   },
   {
     id: 5,
     name: "react",
-    url: "https://live.staticflickr.com/65535/50386891036_f59f2beb43_o.png",
+    icon: <SiReact />,
   },
   {
     id: 6,
     name: "redux",
-    url: "https://live.staticflickr.com/65535/51871383649_9ac93af9d1_o.png",
+    icon: <SiRedux />,
   },
   {
     id: 7,
     name: "nextjs",
-    url: "https://live.staticflickr.com/65535/51871066051_07b5f267e6_b.jpg",
+    icon: <SiNextdotjs />,
   },
   {
     id: 8,
     name: "styled-components",
-    url: "https://live.staticflickr.com/65535/51870105767_4d2cdaefc9_o.png",
+    icon: <SiStyledcomponents />,
   },
   {
     id: 9,
     name: "sass",
-    url: "https://live.staticflickr.com/65535/51871147533_8f0302182c_o.png",
+    icon: <SiSass />,
   },
   {
     id: 10,
     name: "git",
-    url: "https://live.staticflickr.com/65535/50387115033_1b5c8fa4af_o.png",
+    icon: <SiGit />,
   },
   {
     id: 11,
     name: "mongodb",
-    url: "https://live.staticflickr.com/65535/50386908296_fd0076d834_o.png",
+    icon: <SiMongodb />,
   },
   {
     id: 12,
     name: "mysql",
-    url: "https://live.staticflickr.com/65535/50386226653_5a9046ec0d_o.png",
+    icon: <SiMysql />,
   },
   {
     id: 13,
     name: "nodejs",
-    url: "https://live.staticflickr.com/65535/50386898781_b10d01066f_o.png",
+    icon: <SiNodedotjs />,
   },
   {
     id: 14,
     name: "python",
-    url: "https://live.staticflickr.com/65535/50386222168_fb783afb04_o.png",
+    icon: <SiPython />,
   },
 ];
 
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,17 +6,67 @@ import Image from "next/image";
 const MySkills = styled.div``;
 
 const List = styled.ul`
-  /* background-color: var(--black); // todo: change color */
   display: grid;
   grid-template-columns: auto auto auto auto auto auto auto;
   width: 100%;
   height: 200px;
 `;
 
-const Logo = styled.li`
+const IconWrapper = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   list-style: none;
-  width: 75px;
-  height: 75px;
+  font-size: 4rem;
+
+  .name {
+    color: var(--heading);
+    margin-top: 20px;
+  }
+
+  &.javascript {
+    color: rgb(240, 219, 78);
+  }
+  &.typescript {
+    color: rgb(0, 122, 205);
+  }
+  &.html {
+    color: rgb(229, 77, 38);
+  }
+  &.css {
+    color: rgb(49, 99, 168);
+  }
+  &.react {
+    color: rgb(96, 213, 250);
+  }
+  &.redux {
+    color: rgb(111, 79, 182);
+  }
+  &.nextjs {
+    color: rgb(255, 255, 255);
+  }
+  &.styled-components {
+    color: rgb(255, 255, 255);
+  }
+  &.sass {
+    color: rgb(193, 109, 152);
+  }
+  &.git {
+    color: rgb(223, 92, 64);
+  }
+  &.mongodb {
+    color: rgb(71, 138, 76);
+  }
+  &.mysql {
+    color: rgb(65, 101, 171);
+  }
+  &.nodejs {
+    color: rgb(102, 155, 79);
+  }
+  &.python {
+    color: rgb(64, 109, 155);
+  }
 `;
 
 export default function Skills() {
@@ -24,17 +74,12 @@ export default function Skills() {
     <MySkills>
       <Heading text="Skills" />
       <List>
-        {skills.map(({ id, name, url }) => {
+        {skills.map(({ id, name, icon }) => {
           return (
-            <Logo key={id}>
-              <Image
-                src={url}
-                alt={name}
-                width={50}
-                height={50}
-                objectFit="contain"
-              />
-            </Logo>
+            <IconWrapper key={id} className={name}>
+              {icon}
+              <p className="name">{name}</p>
+            </IconWrapper>
           );
         })}
       </List>

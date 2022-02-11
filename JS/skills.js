56bb8a32ed4c8ef34ee7b04bc21791064784d46a
Abel Zambrano <azambrano1417@gmail.com>
2022-02-10 23:20:19 -0800

    add skill logos

---
+++ JS/skills.js
@@ -0,0 +1,74 @@
+const skills = [
+  {
+    id: 1,
+    name: "javascript",
+    url: "https://live.staticflickr.com/65535/51871383674_a5c9a49a24_b.jpg",
+  },
+  {
+    id: 2,
+    name: "typescript",
+    url: "https://live.staticflickr.com/65535/51870105752_dff6a15a8f_o.png",
+  },
+  {
+    id: 3,
+    name: "html",
+    url: "https://live.staticflickr.com/65535/51871714935_28ccd9605d_o.png",
+  },
+  {
+    id: 4,
+    name: "css",
+    url: "https://live.staticflickr.com/65535/51871714940_84725e0c0f_b.jpg",
+  },
+  {
+    id: 5,
+    name: "react",
+    url: "https://live.staticflickr.com/65535/50386891036_f59f2beb43_o.png",
+  },
+  {
+    id: 6,
+    name: "redux",
+    url: "https://live.staticflickr.com/65535/51871383649_9ac93af9d1_o.png",
+  },
+  {
+    id: 7,
+    name: "nextjs",
+    url: "https://live.staticflickr.com/65535/51871066051_07b5f267e6_b.jpg",
+  },
+  {
+    id: 8,
+    name: "styled-components",
+    url: "https://live.staticflickr.com/65535/51870105767_4d2cdaefc9_o.png",
+  },
+  {
+    id: 9,
+    name: "sass",
+    url: "https://live.staticflickr.com/65535/51871147533_8f0302182c_o.png",
+  },
+  {
+    id: 10,
+    name: "git",
+    url: "https://live.staticflickr.com/65535/50387115033_1b5c8fa4af_o.png",
+  },
+  {
+    id: 11,
+    name: "mongodb",
+    url: "https://live.staticflickr.com/65535/50386908296_fd0076d834_o.png",
+  },
+  {
+    id: 12,
+    name: "mysql",
+    url: "https://live.staticflickr.com/65535/50386226653_5a9046ec0d_o.png",
+  },
+  {
+    id: 13,
+    name: "nodejs",
+    url: "https://live.staticflickr.com/65535/50386898781_b10d01066f_o.png",
+  },
+  {
+    id: 14,
+    name: "python",
+    url: "https://live.staticflickr.com/65535/50386222168_fb783afb04_o.png",
+  },
+];
+
+export default skills;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,19 +1,43 @@
 import styled from "styled-components";
 import Heading from "./Heading";
+import skills from "../JS/skills";
+import Image from "next/image";
 
 const MySkills = styled.div``;
 
-const Wrapper = styled.div`
-  background-color: var(--black);
+const List = styled.ul`
+  /* background-color: var(--black); // todo: change color */
+  display: grid;
+  grid-template-columns: auto auto auto auto auto auto auto;
   width: 100%;
   height: 200px;
+`;
+
+const Logo = styled.li`
+  list-style: none;
+  width: 75px;
+  height: 75px;
 `;
 
 export default function Skills() {
   return (
     <MySkills>
       <Heading text="Skills" />
-      <Wrapper></Wrapper>
+      <List>
+        {skills.map(({ id, name, url }) => {
+          return (
+            <Logo key={id}>
+              <Image
+                src={url}
+                alt={name}
+                width={50}
+                height={50}
+                objectFit="contain"
+              />
+            </Logo>
+          );
+        })}
+      </List>
     </MySkills>
   );
 }

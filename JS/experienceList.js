e34c9464d8227e52d6c795113fa42f1de48bb760
Abel Zambrano <azambrano1417@gmail.com>
2022-03-01 18:42:32 -0800

    add experience data

---
+++ JS/experienceList.js
@@ -0,0 +1,37 @@
+const experienceList = [
+  {
+    id: 1,
+    company: "Hack for LA",
+    image:
+      "https://res.cloudinary.com/dgbnqrc5j/image/upload/v1646186721/Experience/HackforLA_j4shyf.png",
+    location: "Los Angeles, CA",
+    url: "https://www.hackforla.org",
+    github: "https://github.com/Abel-Zambrano/website",
+    tech: ["JavaScript", "Jekyll", "Docker", "SASS", "HTML", "CSS"],
+    summary: "updating soon...",
+  },
+  {
+    id: 2,
+    company: "Army of Volunteers for Earth",
+    image:
+      "https://res.cloudinary.com/dgbnqrc5j/image/upload/v1646186721/Experience/ArmyofVolunteers_weuimm.png",
+    location: "San Jose, CA",
+    url: "https://armyofvolunteers.org/",
+    github: null,
+    tech: ["Wordpress", "Elementor", "Google Forms", "HTML", "CSS"],
+    summary: "updating soon...",
+  },
+  {
+    id: 3,
+    company: "AN Janitorial Services",
+    image:
+      "https://res.cloudinary.com/dgbnqrc5j/image/upload/v1646186720/Experience/ANJanitorial_pwght0.png",
+    location: "San Leandro, CA",
+    url: "https://www.anjanitorialservice.com/",
+    github: "https://github.com/Abel-Zambrano/an-janitorial-service",
+    tech: ["React", "JavaScript", "EmailJS", "SASS", "HTML", "CSS"],
+    summary: "updating soon...",
+  },
+];
+
+export default experienceList;

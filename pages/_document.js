8829b855f6cbcafa57dfc31d7a70fd4dfa288386
Abel Zambrano <azambrano1417@gmail.com>
2022-03-28 19:15:56 -0700

    fix cross0rigin

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
             rel="stylesheet"

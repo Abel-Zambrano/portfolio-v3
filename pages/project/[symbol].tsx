b8fc28f0ac42ecf6d14611ab5cc705f12407ccac
Abel Zambrano <azambrano1417@gmail.com>
2022-03-28 19:40:49 -0700

    update

--- pages/project/[symbol].tsx
+++ pages/project/[symbol].tsx
@@ -27,16 +27,16 @@ export default function Symbol() {
     <Page>
       <MyInfo>
         <Card
-          id={result[0].id}
-          symbol={result[0].symbol}
-          company={result[0].company}
-          image={result[0].image}
-          location={result[0].location}
-          url={result[0].url}
-          github={result[0].github}
-          tech={result[0].tech}
-          summary={result[0].summary}
-          contributions={result[0].contributions}
+          id={result[0]?.id}
+          symbol={result[0]?.symbol}
+          company={result[0]?.company}
+          image={result[0]?.image}
+          location={result[0]?.location}
+          url={result[0]?.url}
+          github={result[0]?.github}
+          tech={result[0]?.tech}
+          summary={result[0]?.summary}
+          contributions={result[0]?.contributions}
         />
       </MyInfo>
     </Page>

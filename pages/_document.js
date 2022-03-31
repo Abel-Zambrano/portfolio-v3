49fea633feebd473eee2b982ac456f8611fbd9eb
Abel Zambrano <azambrano1417@gmail.com>
2022-03-30 17:22:01 -0700

    update for styled-components injection

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,57 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
+
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const initialProps = await Document.getInitialProps(ctx);
+//     return { ...initialProps };
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel="preconnect" href="https://fonts.googleapis.com" />
+//           <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin />
+//           <link
+//             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
+//             rel="stylesheet"
+//           />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }

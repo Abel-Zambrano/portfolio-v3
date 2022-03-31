e5e5106d53772b75ca243d9ccaec585e84f94845
Abel Zambrano <azambrano1417@gmail.com>
2022-03-30 18:42:38 -0700

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LJ5F3C3HBH"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LJ5F3C3HBH');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
